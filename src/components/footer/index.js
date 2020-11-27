0f3bf537ffb6cd4d19a65cecb9ee5b96aa7842cf
Ahmad Shafiq <mahmadshafiq786@gmail.com>
2020-11-27 03:07:05 -0800

    Exclude social media links

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -6,13 +6,14 @@ export default class Footer extends Component {
       <footer class="main-footer">
         <div class="container">
           <div class="row">
-            <div class="col-md-6 text-center text-lg-left">
-              <p class="social"><a href="#" class="external facebook wow fadeInUp"><i class="fa fa-facebook"></i></a><a
-                href="#" data-wow-delay="0.2s" class="external instagram wow fadeInUp"><i
-                  class="fa fa-instagram"></i></a><a href="#" data-wow-delay="0.4s"
-                    class="external gplus wow fadeInUp"><i class="fa fa-google-plus"></i></a><a href="#"
-                      data-wow-delay="0.6s" class="email wow fadeInUp"><i class="fa fa-envelope"></i></a></p>
-            </div>
+            {/* <div class="col-md-6 text-center text-lg-left">
+              <p class="social">
+                <a href="#" class="external facebook wow fadeInUp"><i class="fa fa-facebook"></i></a>
+                <a href="#" data-wow-delay="0.2s" class="external instagram wow fadeInUp"><i class="fa fa-instagram"></i></a>
+                <a href="#" data-wow-delay="0.4s" class="external gplus wow fadeInUp"><i class="fa fa-google-plus"></i></a>
+                <a href="#" data-wow-delay="0.6s" class="email wow fadeInUp"><i class="fa fa-envelope"></i></a>
+                </p>
+            </div> */}
             <div class="col-md-6 text-center text-lg-right mt-4 mt-lg-0">
               <p>© 2020 Jazzari. All rights reserved.</p>
             </div>
