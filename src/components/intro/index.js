9df0abdd43bd28af4b1928e53e7ac7f0299b1cf6
Ahmad Shafiq <mahmadshafiq786@gmail.com>
2020-11-28 05:20:06 -0800

    Added animation on intro

--- src/components/intro/index.js
+++ src/components/intro/index.js
@@ -1,16 +1,20 @@
-import React, { Component } from 'react';
+import React from 'react';
+import './style.css';
 
-export default class Intro extends Component {
-  render() {
+export default () => {
+  return (
+    <div id="intro">
+      <video autoPlay muted loop className="background-video" poster="animations/blue-plexus.jpg">
+        <source src="animations/blue-plexus.mov" type="video/mp4" />
+      </video>
 
-    return (
-      <section id="intro" style={{ "background": "url(img/home.jpg) center center no-repeat", "backgroundSize": "cover" }} class="intro-section pb-2" >
-        <div class="container text-center">
-          <div data-animate="fadeInDown" class="logo"><img src="img/logo-big.png" alt="logo" width="250" /></div>
-          <h1 data-animate="fadeInDown" class="text-shadow mb-5">Complex Problems, Simple Solutions</h1>
-          {/* <p data-animate="slideInUp" class="h3 text-shadow text-400">I grind HTML and CSS and then weld them with PHP into beautiful and efficient websites.</p> */}
+      <div className="container text-center foreground" >
+        <div data-animate="fadeInDown" class="logo">
+          <img className="logo-image" src="img/logo/full-logo.png" alt="logo" />
         </div>
-      </section>
-    );
-  }
+        <h1 data-animate="fadeInDown" class="text-shadow mb-5">Complex Problems, Simple Solutions</h1>
+        {/* <p data-animate="slideInUp" class="h3 text-shadow text-400">I grind HTML and CSS and then weld them with PHP into beautiful and efficient websites.</p> */}
+      </div>
+    </div>
+  );
 }
